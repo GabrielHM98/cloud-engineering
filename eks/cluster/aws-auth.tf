945b223d1a300ed0c32b0c0a8138484fca992604
GabrielHM98 <gabe.hmartins98@gmail.com>
2026-02-21 23:14:28 -0300

    fix

--- eks/cluster/aws-auth.tf
+++ eks/cluster/aws-auth.tf
@@ -1,9 +1,6 @@
 module "eks_aws_auth" {
   source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
   version = "~> 20.0"
-
-  cluster_name = module.eks.cluster_name
-
   aws_auth_users = [
     {
       userarn  = "arn:aws:iam::438987839708:user/admin"
