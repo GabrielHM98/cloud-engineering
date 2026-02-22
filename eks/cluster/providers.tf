82022457cb2f9bb74c4c91588c4db4f5de595253
GabrielHM98 <gabe.hmartins98@gmail.com>
2026-02-22 13:09:38 -0300

    fix

--- eks/cluster/providers.tf
+++ eks/cluster/providers.tf
@@ -6,5 +6,27 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
