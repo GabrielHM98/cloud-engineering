d6592c8e651bdc6d87661abfdbb52a538055d544
GabrielHM98 <gabe.hmartins98@gmail.com>
2026-02-22 16:45:27 -0300

    fix

--- eks/cluster/init/nodepools.tf
+++ eks/cluster/init/nodepools.tf
@@ -5,9 +5,9 @@ locals {
       instance_types = ["t3.small", "t3a.small"]
       capacity_type  = "SPOT"
 
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      min_size     = 3
+      max_size     = 3
+      desired_size = 3
 
       labels = {
         role = "platform"
