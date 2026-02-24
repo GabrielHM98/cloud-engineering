b93606e81f4254d235659f70ba8d0f55aa6f004f
GabrielHM98 <gabe.hmartins98@gmail.com>
2026-02-23 21:57:10 -0300

    fix

--- aks/cluster/init/outputs.tf
+++ aks/cluster/init/outputs.tf
@@ -2,10 +2,6 @@ output "cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "cluster_endpoint" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].host
-}
-
 output "resource_group" {
   value = azurerm_resource_group.aks.name
 }
